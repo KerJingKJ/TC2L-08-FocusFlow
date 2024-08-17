b8103e4fa73fc4687884d6525a6c1a999bc73df3
Ker Jing <jingkerchan@gmail.com>
2024-08-17 17:21:29 +0800

    Did some Homepage coding

--- homepage/views.py
+++ homepage/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(response):
-    return HttpResponse("Welcome to FocusFlow!")
+def homepage(response):
+    return render(response, 'homepage/homepage.html')

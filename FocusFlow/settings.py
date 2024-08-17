c9a249e26a43a492adafb2d95537700b6b9225ea
Ker Jing <jingkerchan@gmail.com>
2024-08-17 18:44:39 +0800

    added goals page

--- FocusFlow/settings.py
+++ FocusFlow/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'homepage.apps.HomepageConfig',
-    'goals.apps.GoalsConfig'
+    'goals.apps.GoalsConfig',
 ]
 
 MIDDLEWARE = [

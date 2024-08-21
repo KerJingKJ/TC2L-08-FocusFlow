6fcbfc8298bbfe87a14290f5f547110a910a03b5
BerniceGan <bernicegxinyi@gmail.com>
2024-08-21 13:56:09 +0800

    I do smtg

--- accounts/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-
-from django import forms
-from django.contrib.auth.models import User
-
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'first_name', 'last_name')

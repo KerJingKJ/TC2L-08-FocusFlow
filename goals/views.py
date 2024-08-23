4d7e3df7b460271f5e942b85ce1e36cbbf57bf8a
Ker Jing <jingkerchan@gmail.com>
2024-08-23 13:58:42 +0800

    goals

--- goals/views.py
+++ goals/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from .models import ToDoList
+from .forms import GoalForm
 # Create your views here.
 
 def goals(response):
+    form = ToDoList()
     return render(response,'goals/goals.html')
+
+

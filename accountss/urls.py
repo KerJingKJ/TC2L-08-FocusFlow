c6605c315f3ec0943ee2ed25c0b5e934a0bee272
BerniceGan <bernicegxinyi@gmail.com>
2024-08-25 17:09:58 +0800

    I finally can see my accountss

--- accountss/urls.py
+++ accountss/urls.py
@@ -1,9 +1,10 @@
 # accountss/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.signup, name='signup'),  # or some other view
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
 ]
-

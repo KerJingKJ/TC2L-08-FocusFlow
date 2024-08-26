89191a6e00b14c5dcaf0922f70ef4d5fc66d7cb2
BerniceGan <bernicegxinyi@gmail.com>
2024-08-26 16:03:12 +0800

    I just saved smtg else

--- accountss/forms.py
+++ accountss/forms.py
@@ -1,13 +1,53 @@
+# accounts/forms.py
+# from django import forms
+# from django.contrib.auth.models import User
+# from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+# class CustomUserCreationForm(UserCreationForm):
+    # class Meta:
+        # model = User
+        # fields = ['username', 'email', 'password1', 'password2']
+
+# class CustomUserChangeForm(UserChangeForm):
+    # class Meta:
+        # model = User
+        # fields = ['username', 'email']
+
+# class SignUpForm(UserCreationForm):
+    # email = forms.EmailField(max_length=200, help_text='Required')
+
+    # class Meta:
+        # model = User
+        # fields = ('username', 'email', 'password1', 'password2')
+
+
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    first_name = forms.CharField(max_length=100, help_text='Optional')
+    last_name = forms.CharField(max_length=100, help_text='Optional')
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
 
 class CustomUserChangeForm(UserChangeForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    first_name = forms.CharField(max_length=100, help_text='Optional')
+    last_name = forms.CharField(max_length=100, help_text='Optional')
+
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ('username', 'email', 'first_name', 'last_name')
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    first_name = forms.CharField(max_length=100, help_text='Optional')
+    last_name = forms.CharField(max_length=100, help_text='Optional')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
--- mood/urls.py
+++ mood/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('mood/', views.mood_view, name="mood")
+    path('', views.mood_view, name="mood")
 ]

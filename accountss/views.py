394c35cff008565feab9251e8d32d79c627faac7
BerniceGan <bernicegxinyi@gmail.com>
2024-08-27 23:02:38 +0800

    I change the title

--- accountss/views.py
+++ accountss/views.py
@@ -1,44 +1,5 @@
 # accountss/views.py
 
-#from django.shortcuts import render, redirect
-#from django.contrib.auth import authenticate, login
-#from django.contrib.auth import login
-#from django.contrib import messages
-#from .forms import SignUpForm
-
-#def login_view(request):
-    #if request.method == 'POST':
-        #username = request.POST['username']
-        #password = request.POST['password']
-        
-        # Validate the form
-        #if not username or not password:
-            #messages.error(request, 'Please enter both username and password.')
-            #return render(request, 'accountss/login.html')
-        
-        #user = authenticate(request, username=username, password=password)
-        #if user is not None:
-            #login(request, user)
-            #return redirect('dashboard')  # Redirect to the dashboard after login
-        #else:
-            #messages.error(request, 'Invalid username or password.')
-            #return render(request, 'accountss/login.html')
-    #return render(request, 'accountss/login.html')
-
-#def signup(request):
-    #if request.method == 'POST':
-        #form = SignUpForm(request.POST)
-        #if form.is_valid():
-            #user = form.save()
-            #login(request, user)
-            #messages.success(request, 'Registration successful!')
-            #return redirect('dashboard')  # Redirect to the dashboard after registration
-        #else:
-            #messages.error(request, 'Please correct the errors below.')
-    #else:
-        #form = SignUpForm()
-    #return render(request, 'accountss/signup.html', {'form': form})
-
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
@@ -55,7 +16,7 @@ def login_view(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                return redirect('dashboard')
+                return redirect('homepage/homepage.html')
             else:
                 messages.error(request, 'Your account is not active.')
                 return render(request, 'accountss/login.html')

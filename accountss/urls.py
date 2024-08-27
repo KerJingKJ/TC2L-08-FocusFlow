5668cacbd9683aa76b0f3bf6f95f7341ac963f92
BerniceGan <bernicegxinyi@gmail.com>
2024-08-27 11:01:56 +0800

    I changed the layout of the accountss only first

--- accountss/urls.py
+++ accountss/urls.py
@@ -4,15 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.signup, name='signup'),  # or some other view
-    path('signup/', views.signup, name='signup'),
+    path('', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
-]
-
-# new one
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
 ]

1e10c3f99c91fe3e897b513f184fe533d5e34022
BerniceGan <bernicegxinyi@gmail.com>
2024-08-28 13:29:56 +0800

    Updates of latest

--- FocusFlow/urls.py
+++ FocusFlow/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from account import views as v
+from accountss import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),

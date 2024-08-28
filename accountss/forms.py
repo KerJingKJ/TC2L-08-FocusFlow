934d589797af200e72167dc72b49ab1b109a3d81
BerniceGan <bernicegxinyi@gmail.com>
2024-08-28 13:16:53 +0800

    Updating

--- accountss/forms.py
+++ accountss/forms.py
@@ -30,3 +30,37 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+#from django import forms
+#from .models import CustomUser
+#from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+
+#class CustomUserCreationForm(UserCreationForm):
+    #email = forms.EmailField(max_length=200, help_text='Required')
+    #first_name = forms.CharField(max_length=100, help_text='Optional')
+    #last_name = forms.CharField(max_length=100, help_text='Optional')
+
+    #class Meta:
+        #model = CustomUser
+        #fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+
+#class CustomUserChangeForm(UserChangeForm):
+    #email = forms.EmailField(max_length=200, help_text='Required')
+    #first_name = forms.CharField(max_length=100, help_text='Optional')
+    #last_name = forms.CharField(max_length=100, help_text='Optional')
+
+    #class Meta:
+        #model = CustomUser
+        #fields = ('username', 'email', 'first_name', 'last_name')
+
+
+#class SignUpForm(UserCreationForm):
+    #email = forms.EmailField(max_length=200, help_text='Required')
+    #first_name = forms.CharField(max_length=100, help_text='Optional')
+    #last_name = forms.CharField(max_length=100, help_text='Optional')
+
+    #class Meta:
+        #model = CustomUser
+        #fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')

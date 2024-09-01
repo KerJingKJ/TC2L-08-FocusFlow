1bdd1b9e5147bd1e268c7bfe639ec21087f6cd74
BerniceGan <bernicegxinyi@gmail.com>
2024-09-01 22:55:35 +0800

    I updated for this and next week

--- accountss/urls.py
+++ accountss/urls.py
@@ -6,5 +6,14 @@ from . import views
 urlpatterns = [
     path('', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
+    path('homepage/', views.homepage, name='homepage'),
+   
 ]
 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile_view, name='profile'),
+]

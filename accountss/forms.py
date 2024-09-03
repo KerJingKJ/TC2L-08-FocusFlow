aaba28946bc3dcf0582a18a986fa6c015d3c2357
BerniceGan <bernicegxinyi@gmail.com>
2024-09-03 22:56:53 +0800

    Updates on my week 3 and 4

--- accountss/forms.py
+++ accountss/forms.py
@@ -1,7 +1,10 @@
 # accounts/forms.py
+
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
+from .models import Profile
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required', widget=forms.EmailInput())
@@ -16,6 +19,7 @@ class CustomUserCreationForm(UserCreationForm):
             'password2': 'Confirm your password',
         }
 
+
 class CustomUserChangeForm(UserChangeForm):
     email = forms.EmailField(max_length=200, help_text='Required', widget=forms.EmailInput())
     first_name = forms.CharField(max_length=100, help_text='Optional')
@@ -25,22 +29,19 @@ class CustomUserChangeForm(UserChangeForm):
         model = User
         fields = ('username', 'email', 'first_name', 'last_name')
 
+
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(max_length=150)
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-
-# forms.py
-from django import forms
-from django.contrib.auth.models import User
-
 class ProfileForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email')
+        model = Profile
+        fields = ('bio', 'location', 'avatar')

e3a1036918489233d502c1589391845cfab199e6
BerniceGan <bernicegxinyi@gmail.com>
2024-09-04 14:16:27 +0800

    updates

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name="homepage"),
-    path('home/', views.homepage, name="homepage")
+    path('', views.homepage, name="home")
 ]

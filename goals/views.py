003da50cc9315dc11150e40b041b437917d4ea49
Ker Jing <jingkerchan@gmail.com>
2024-09-05 21:23:53 +0800

    done feature for music playlist

--- goals/views.py
+++ goals/views.py
@@ -3,13 +3,23 @@ from django.contrib.auth.decorators import login_required
 from .models import ToDoList
 from .forms import GoalForm
 from motivation.models import Actor, Quote
+from playlist.models import Playlist, Track
 # Create your views here.
 
+#for to do list settings
 @login_required
 def set_goals(request):
     goals = ToDoList.objects.filter(user=request.user) 
     actors = Actor.objects.all()
     quotes = Quote.objects.all()
+    playlists = Playlist.objects.all()
+    tracks = Track.objects.all()
+
+# added the view to display it on goals page too when user select a playlist
+    selected_playlist = None
+    playlist_id = request.GET.get('playlist_id')
+    if 'playlist_id' in request.GET and playlist_id.isdigit():
+        selected_playlist = get_object_or_404(Playlist, id=request.GET['playlist_id'])
     if request.method == 'POST':
         form = GoalForm(request.POST)
         if form.is_valid():
@@ -19,7 +29,8 @@ def set_goals(request):
             return redirect('set_goals')
     else:
         form = GoalForm()
-   #added status bar to sum up the total goals has completed / not completed
+
+# added status bar to sum up the total goals has completed / not completed
     done = 0
     notdone = 0
     total = 0
@@ -44,6 +55,9 @@ def set_goals(request):
             'total':total,
             'actors':actors,
             'quotes':quotes,
+            'playlists':playlists,
+            'tracks':tracks,
+            'selected_playlist': selected_playlist,
         })
 
 def edit(request, goal_id):

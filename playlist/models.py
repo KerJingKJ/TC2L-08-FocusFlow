ccc058a4f263f947d6767972ed6626b940032d0c
Ker Jing <jingkerchan@gmail.com>
2024-09-05 09:29:44 +0800

    playlist feature

--- playlist/models.py
+++ playlist/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+#Music Playlist model
+class Playlist(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Track(models.Model):
+    playlist = models.ForeignKey(Playlist, related_name='tracks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, default = "Untitled")
+    audio_file = models.FileField(upload_to='tracks/')
+
+    def __str__(self):
+        return self.title
--- playlist/views.py
+++ playlist/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Playlist
 # Create your views here.
 
-def playlist(response):
-    return render(response, 'playlist/playlist.html')
+#def playlist(response):
+ #   return render(response, 'playlist/playlist.html')
+
+def select_playlist(request):
+    playlists = Playlist.objects.all()
+    selected_playlist = None
+
+    if 'playlist_id' in request.GET:
+        selected_playlist = get_object_or_404(Playlist, id=request.GET['playlist_id'])
+
+    return render(request, 'playlist/playlist.html', 
+    {
+        'playlists':playlists,
+        'selected_playlist':selected_playlist,
+    })

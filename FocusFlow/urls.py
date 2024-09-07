fc4c202c5750781bf73d0db9e3128274eaf777e1
Ker Jing <jingkerchan@gmail.com>
2024-09-07 21:05:53 +0800

    resolved conflict

--- FocusFlow/urls.py
+++ FocusFlow/urls.py
@@ -29,12 +29,7 @@ urlpatterns = [
     path('timer/', include("timer.urls")),
     path('motivation/', include("motivation.urls")),
     path('background/', include("background.urls")),
-    path('playlist/', include("playlist.urls"))
-<<<<<<< HEAD
-=======
-
-
->>>>>>> main
+    path('playlist/', include("playlist.urls")),
 ]
 
 if settings.DEBUG:

d2c255d7037c3789805eaff10e3d3e29bfe50962
BerniceGan <bernicegxinyi@gmail.com>
2024-09-09 18:40:28 +0800

    Update for week 5

--- mood/urls.py
+++ mood/urls.py
@@ -2,9 +2,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+ #   path('', views.mood_list, name="mood"),
+ #   path('mood_tracking/', views.mood_tracking, name='mood_tracking'),
+ #   path('log_mood/', views.log_mood, name='log_mood'),
+ #   path('mood_history/', views.mood_history, name='mood_history'),
+ #   path('mood/', views.mood_list, name='mood_list'),
+ #   path('mood/tracking/', views.mood_tracking, name='mood_tracking'),
+ #   path('mood/create/', views.mood_create, name='mood_create'),
+ #   path('mood/history/', views.mood_history, name='mood_history'),
+  #  path('mood/log/', views.log_mood, name='log_mood'),
+ #   path('mood/', views.mood, name='mood'),
+ #   path('mood/list/', views.mood_list, name='mood_list'),
+
     path('', views.mood_list, name="mood"),
     path('mood_tracking/', views.mood_tracking, name='mood_tracking'),
-    path('log_mood/', views.mood_list, name='log_mood'),
+    path('log_mood/', views.log_mood, name='log_mood'),
     path('mood_history/', views.mood_history, name='mood_history'),
-    path('mood/', views.mood_list, name='mood_list'),
+    path('mood/create/', views.mood_create, name='mood_create'),
+    path('mood_list/', views.mood_list, name='mood_list'),
 ]
+
+

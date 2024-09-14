bec97297cdf19ef9f91f6f1c7edda9c83b95793d
Ker Jing <jingkerchan@gmail.com>
2024-09-14 19:07:41 +0800

    now only left with edit

--- goals/views.py
+++ goals/views.py
@@ -14,7 +14,7 @@ def set_goals(request):
     quotes = Quote.objects.all()
     playlists = Playlist.objects.all()
     tracks = Track.objects.all()
-
+    form = GoalForm(request.POST)
 # added the view to display it on goals page too when user select a playlist
     selected_playlist = None
     playlist_id = request.GET.get('playlist_id')
@@ -34,6 +34,7 @@ def set_goals(request):
             goals = form.save(commit=False)
             goals.user = request.user 
             goals.save()
+
             form = GoalForm()
             goals = ToDoList.objects.filter(user=request.user) 
             done = 0
@@ -56,10 +57,12 @@ def set_goals(request):
                 'notdone': notdone,
                 'bar': bar,
                 'total': total,
+                'form':form,
             })   
     else:
         form = GoalForm()
 # added status bar to sum up the total goals has completed / not completed
+
     done = 0
     notdone = 0
     total = 0
@@ -91,17 +94,43 @@ def set_goals(request):
 
 def edit(request, goal_id):
     goals = get_object_or_404(ToDoList, id=goal_id)
+    
     if request.method == 'POST':
         form = GoalForm(request.POST, instance=goals)
         if form.is_valid():
             form.save()
-            return redirect('set_goals')
+            if request.headers.get('HX-Request'):
+                goals = ToDoList.objects.filter(user=request.user) 
+                done = 0
+                notdone = 0
+                total = 0
+                for goal in goals:
+                    if goal.completed == True:
+                        done += 1
+                    else:
+                        notdone += 1
+                total = done + notdone
+                
+                if done > 0:
+                    bar = (done/total)*100
+                else:
+                    bar = 0
+                return render(request, 'goals/goals_content.html', 
+                {
+                    'goals': goals, 
+                    'done':done, 
+                    'notdone':notdone, 
+                    'bar':bar,
+                    'total':total,
+                    'form':form,
+                })
     else:
         form = GoalForm(instance=goals)
-    return render(request, 'goals/goals.html', {'form': form, 'editing': True})
+    return render(request, 'goals/goals_content.html', {'form': form, 'editing': True})
 
 def delete(request, goal_id):
     goals = get_object_or_404(ToDoList, id=goal_id)
+    form = GoalForm()
     if request.headers.get('HX-Request'):
         goals.delete()
         goals = ToDoList.objects.filter(user=request.user) 
@@ -126,11 +155,13 @@ def delete(request, goal_id):
             'notdone':notdone, 
             'bar':bar,
             'total':total,
+            'form':form,
         })
     
 
 def complete(request, goal_id, action):
     goals = get_object_or_404(ToDoList, id=goal_id)
+    form = GoalForm()
     if request.headers.get('HX-Request'):
         if action == "complete":
             goals.completed = True
@@ -156,6 +187,7 @@ def complete(request, goal_id, action):
                 'notdone': notdone,
                 'bar': bar,
                 'total': total,
+                'form':form,
             })   
         else:
             goals.completed = False
@@ -181,6 +213,7 @@ def complete(request, goal_id, action):
                 'notdone': notdone,
                 'bar': bar,
                 'total': total,
+                'form':form,
             })   
 
     return render(request, 'goals/goals_content.html', 
@@ -190,5 +223,6 @@ def complete(request, goal_id, action):
             'notdone':notdone, 
             'bar':bar,
             'total':total,
+            'form':form,
         })
 

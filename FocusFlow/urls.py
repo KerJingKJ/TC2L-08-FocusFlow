d66b5e690f3a61fb6214b63538825b5ae6a3b828
Ker Jing <jingkerchan@gmail.com>
2024-09-23 18:39:04 +0800

    added

--- FocusFlow/urls.py
+++ FocusFlow/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('playlist/', include("playlist.urls")),
     path('', include('accountss.urls')),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
